99181947172cab39ea3d8104eb9eebe4aa26bd88
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-09-13 15:38:08 -0500

    added router to app.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from "react-router-dom";
 
 class App extends React.Component {
+  state = { books: [] };
+
+  async fetchBooks() {
+    let apiUrl = '${process.env.SERVER}/books';
+  }
+  render() {
+    return (
+      <>
+        <Router>
+          <nav>
+            <h1>World of Books</h1>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/">
+              <h1>Home</h1>
+            </Route>
+            <Route path="/about">
+              <h1>About Page Here</h1>
+              <p>I am Jaren</p>
+            </Route>
+          </Switch>
+        </Router>
+      </>
+    )
+  }
+
 
   constructor(props) {
     super(props);

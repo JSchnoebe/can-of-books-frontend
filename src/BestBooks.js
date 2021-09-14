84dd407538d2c2882e7f22adbb5272bec0605a89
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-09-14 14:36:40 -0500

    edited my retun function

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,28 +1,42 @@
+import axios from 'axios';
 import React from 'react';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: null
     }
+  }
+
+  componentDidMount() {
+    this.fetchBooks();
+  }
+
+  async fetchBooks() {
+    const SERVER = process.env.SERVER;
+    let results = await axios.get(`http://localhost:3001/books`);
+    let books = results.data;
+    console.log(results.data);
+    this.setState({ books });
   }
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
 
   render() {
 
+    const books = this.state.books;
+
     /* TODO: render user's books in a Carousel */
 
     return (
       <>
+        <h1>Books!</h1>
+        {books ? <ul>{books.map((book, idx) => (
+          <p key={idx}>{book.title}</p>
+        ))}</ul>: <p>Loading Books..</p>}
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
       </>
     )
   }
